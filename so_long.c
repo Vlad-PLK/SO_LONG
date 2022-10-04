17223fec08d9113e4ae49dd076a8b691e1fe7bdf
Vladimir <61476758+Vlad-PLK@users.noreply.github.com>
2022-10-04 07:55:54 +0200

    Add files via upload

--- so_long.c
+++ so_long.c
@@ -6,192 +6,43 @@
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:02:27 by vpolojie          #+#    #+#             */
-/*   Updated: 2022/08/03 10:53:14 by vpolojie         ###   ########.fr       */
+/*   Updated: 2022/09/03 01:38:20 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-///#include "mlx.h"
+#include "mlx/mlx.h"
 #include "printf/ft_printf.h"
 #include "printf/libft/libft.h"
 #include "get_next_line/get_next_line.h"
+#include "so_long.h"
 #include <fcntl.h>
-
-typedef struct	s_data{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}t_data;
-
-/*int	main(void)
-{
-	void	*mlx;
-	void	*img;
-	char	*relative_path = "./test.xpm";
-	int		img_width;
-	int		img_height;
-	void	*mlx_win;
-	t_data	*img;
-
-	mlx = mlx_init();
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	//mlx_win = mlx_new_window(mlx, 500, 500, "Hello World!");
-	///mlx_loop(mlx);
-	//img.img = mlx_new_image(mlx, 500, 500);
-	//img.addr = mlc_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-}*/
-
-int		ft_check_extension(char *arg)
-{
-	int	i;
-
-	i = (int)ft_strlen(arg);
-	if (arg[i -4] == '.' && arg[i -3] == 'b'
-		&& arg[i -2] == 'e' && arg[i -1] == 'r')
-		return (1);
-	else
-		return (-1);
-}
-
-int		ft_map_rect_check(char **map_lines, int nb_lines)
-{
-	int	j;
-	int	k;
-
-	j = 0;
-	while (j != nb_lines)
-	{
-		k = j +1;
-		while (k != nb_lines)
-		{
-			if ((ft_strlen(map_lines[j]) == ft_strlen(map_lines[k]))
-			&& (ft_strlen(map_lines[j]) -1) >= 4)
-				k++;
-			else
-				return (0);
-		}
-		j++;
-	}
-	return (1);
-}
-
-int		ft_map_one_lst_check(char **map_lines, int nb_lines)
-{
-	int	i;
-
-	i = 0;
-	while (i != (ft_strlen(map_lines[0]) -2))
-	{
-		if(map_lines[0][i] == '1' && map_lines[nb_lines -1][i] == '1')
-			i++;
-		else
-			return (0);
-	}
-	return (1);
-}
-
-int		ft_map_check_cara(char **map_lines, int nb_lines)
-{
-	int	items;
-	int	pos;
-	int	e_xit;
-	int	i;
-	int	j;
-
-	i = 1;
-	items = 0;
-	pos = 0;
-	e_xit = 0;
-	while (i != (nb_lines -1))
-	{
-		if (map_lines[i][0] == '1'
-			&& map_lines[i][ft_strlen(map_lines[i]) -2] == '1')
-		{	
-			j = 1;
-			while (j != (ft_strlen(map_lines[i]) -2))
-			{
-				if (map_lines[i][j] == 'C' || map_lines[i][j] == 'P'
-				|| map_lines[i][j] == 'E' || map_lines[i][j] == '1'
-				|| map_lines[i][j] == '0')
-				{
-					if (map_lines[i][j] == 'C')
-						items++;
-					if (map_lines[i][j] == 'P')
-						pos++;
-					if (map_lines[i][j] == 'E')
-						e_xit++;
-					j++;
-				}
-				else
-					return (0);
-			}
-			i++;
-		}
-		else
-			return (0);
-	}
-	if (pos != 1 || e_xit != 1)
-		return (0);
-	return (1);
-}
-
-int		ft_map_check(char **map_lines, int nb_lines)
-{
-	if (ft_map_rect_check(map_lines, nb_lines) != 1)
-		return (0);
-	if (ft_map_one_lst_check(map_lines, nb_lines) != 1)
-		return (0);
-	if (ft_map_check_cara(map_lines, nb_lines) != 1)
-		return (0);
-	return (1);
-}
 
 void	so_long(char *map)
 {
 	int		fd;
-	int		i;
 	int		line;
+	int		i;
 	char	**map_lines;
 
 	i = 0;
 	line = 0;
+	map_lines = NULL;
 	if (ft_check_extension(map) == 1)
 	{
 		fd = open(map, O_RDONLY);
-		while(get_next_line(fd) != NULL)
+		while (get_next_line_v2(fd) != 0)
 			i++;
 		close(fd);
 		fd = open(map, O_RDONLY);
 		if (i >= 4)
-			map_lines = (char **)malloc(sizeof(char *) * (i +1));
+			ft_map_init(map_lines, i, line, fd);
 		else
-		{
-			ft_printf("Error\n");
-			exit(EXIT_FAILURE);
-		}
-		map_lines[i +1] = 0;
-		while(i != 0)
-		{
-			map_lines[line] = ft_strdup(get_next_line(fd));
-			line++;
-			i--;
-		}
-		if (ft_map_check(map_lines, line) == 1)
-			ft_printf("1\n");
-		else
-		{
-			ft_printf("Error\n");
-			exit(EXIT_FAILURE);
-		}
+			ft_error_func(map_lines);
 	}
 	else
-	{
-		ft_printf("Error\n");
-		exit(EXIT_FAILURE);
-	}
+		ft_error_func(map_lines);
 }
 
 int	main(int argc, char **argv)
--- so_long.h
+++ so_long.h
@@ -1,9 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 15:48:42 by vpolojie          #+#    #+#             */
+/*   Updated: 2022/09/30 13:53:52 by vpolojie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_stack
+{
+	int	size_max;
+	int	top_index;
+	int	*tableau;
+}t_stack;
+
+typedef struct s_data
+{
+	int	i;
+	int	j;
+	int	player_i;
+	int	player_j;
+	int	exit_i;
+	int	exit_j;
+}t_data;
+
+typedef struct s_ele
+{
+	int	items;
+	int	pos;
+	int	e_xit;
+	int	i;
+	int	j;
+}t_ele;
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*mlx_win;
+	int		x;
+	int		y;
+	int		x_door;
+	int		y_door;
+	int		x_prev;
+	int		y_prev;
+	int		i;
+	int		j;
+	void	*img;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+	int		items;
+	char	**map;
+	int		nb_pas;
+}t_mlx;
+
+typedef struct s_img
+{
+    void	*img;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+	int		x;
+	int		y;
+	int		i;
+	int		j;
+}t_img;
+
+int		ft_map_check(char **map_lines, int nb_lines);
 int		ft_check_extension(char *arg);
+int		ft_map_rect_check(char **map_lines, int nb_lines);
+int		ft_map_one_lst_check(char **map_lines, int nb_lines);
+int		ft_map_check_cara(char **map_lines, int nb_lines);
+int		ft_close_key(int keycode, t_mlx *mlx);
+int		ft_close_mouse(t_mlx *mlx);
+int		ft_loop_cara(t_mlx *mlx);
+int		get_next_line_v2(int fd);
+int		ft_check_wasd(int keycode, t_mlx *mlx);
+int		ft_find_path(char **map_lines, int nb_lines);
+t_img	*ft_cara(t_mlx *mlx);
+t_img	*ft_door(t_mlx *mlx);
+t_img	*ft_collct(t_mlx *mlx);
+t_img	*ft_floor(t_mlx *mlx);
+t_img	*ft_wall(t_mlx *mlx);
+void	ft_map_init(char **map_lines, int i, int line, int fd);
+void	ft_free_wasd_show_result(t_mlx *mlx, t_img *door);
+void	ft_wasd_move_part2(int keycode, t_mlx *mlx, t_img *door);
+void	ft_wasd_move(int keycode, t_mlx *mlx, t_img *door);
+void	ft_wasd_loop(t_mlx *mlx, int keycode);
+void	ft_error_func(char **map);
+void	ft_display_map(char	**map, int nb_lines);
+void	ft_display_others(t_mlx *mlx, t_img *mur, char **map);
+void	ft_display_all(t_mlx *mlx, t_img *mur, char **map);
+void	ft_check_close(t_mlx *mlx);
+void	ft_free_img(t_img *sol, t_img *collct, t_img *door);
 void	so_long(char *map);
+void	create_mlx(t_mlx *mlx, char **map);
 
 #endif
